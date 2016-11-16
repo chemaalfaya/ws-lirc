8d0dc16f305a75f54c03a4590ead00958eb4cedb
chemaalfaya <chemaalfaya@gmail.com>
2016-11-16 12:42:29 +0100

    Initial commit

---
+++ scripts/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "+ Installing required 'avahi' library packet"
+sudo apt-get -y install libavahi-compat-libdnssd-dev
---
+++ scripts/postinstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "+ Updating npm modules dependencies recursively"
+npm update
